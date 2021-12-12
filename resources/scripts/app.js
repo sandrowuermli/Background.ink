4758f97b2f48bfe7f2b3ae0d4d169014cb20c829
Sandro Würmli <mail@sandrowuermli.ch>
2021-12-12 15:38:49 +0100

    Mousemove implementation

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -6,12 +6,47 @@ import 'jquery';
 import screenfull from 'screenfull';
 
 $( () => {
-  const element = $( '#fullscreen' )[0]; // Get DOM element from jQuery collection
+  // Fullscreen
+  const element = $( '#display' )[0]; // Get DOM element from jQuery collection
 
-  $( '#button' ).on( 'click', () => {
+  $( '#fullscreen' ).on( 'click', () => {
     if ( screenfull.isEnabled ) {
       screenfull.request( element );
     }
+  } );
+
+  // Mouse Move
+  $( function() {
+    // When page loads, wait 3 seconds and hide all elements with body class:
+    setTimeout( remove, 3000 );
+  } );
+
+  var timer = null;
+
+  // General function for adding/removing the "hide" class.
+  // This is used when the page first loads and each time
+  // the mouse moves on the page. We're not calling toggle()
+  // here because a flicker effect can happen which would leave
+  // the elements showing instead of being hidden.
+  function toggle() {
+    $( 'body' ).toggleClass( 'mousemove' );
+  }
+
+  function remove() {
+    $( 'body' ).removeClass( 'mousemove' );
+  }
+
+  $( window ).on( 'mousemove', function() {
+    // When anywhere on page is moused over bring back body
+    // elements for 3 seconds. Removing "hide" simply restores
+    // the original CSS & layout
+    $( 'body' ).addClass( 'mousemove' );
+
+    // Kill any previous timers
+    clearTimeout( timer );
+
+    // Wait 3 seconds and hide again
+    timer = setTimeout( remove, 3000 );
   } );
 
 } );
